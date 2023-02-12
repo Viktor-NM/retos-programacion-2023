8513c9c61393ba36821bbc691cd34ca04794af6c
Victor Nuñez <74278016+Viktor-NM@users.noreply.github.com>
2023-02-11 20:13:00 -0600

    Viktor-NM0.js
    Reto #0 Javascript Viktor-NM

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/Viktor-NM-0.js
@@ -0,0 +1,18 @@
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+// function 
+const numbers = Array.from({length: 50}, (x, i) => {
+    if ((i+1) % 3 === 0 ) return 'fizz'
+    if ((i+1) % 5 === 0 ) return 'buzz'
+    if ((i+1) % 3 === 0 && (i+1) % 5 === 0) return 'fizzbuzz'
+    return (i+1)
+})
+
+numbers.map(number => console.log(number))
